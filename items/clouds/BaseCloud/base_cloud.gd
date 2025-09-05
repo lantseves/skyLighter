28a4cc6d69fe5820e76757adf5645ccfb2d266b5
Lantsev Evgeny <e.lantsev@s7.ru>
2025-09-05 11:33:50 +0300

    add coin by cloud

--- items/clouds/BaseCloud/base_cloud.gd
+++ items/clouds/BaseCloud/base_cloud.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
 @export var amountSeconds: int = 1
+@export var max_coin: int = 1
+@export var min_coin: int = 1
 
 func _on_body_entered(body: Node2D) -> void:
-	InGameVars.remaining_timer += amountSeconds	
+	InGameVars.remaining_timer += amountSeconds
+	InGameVars.score += randi_range(min_coin, max_coin)
 	self.queue_free()
