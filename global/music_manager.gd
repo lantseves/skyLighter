d78279bb4abc32f5ed1eadec996bd37fca27c7fe
Lantsev Evgeny <e.lantsev@s7.ru>
2025-12-03 00:39:21 +0300

    build

--- global/music_manager.gd
+++ global/music_manager.gd
@@ -7,24 +7,24 @@ var music_player: AudioStreamPlayer = null
 var music_started: bool = false
 
 func _ready() -> void:
-	# Получаем AudioStreamPlayer из сцены через get_node для надежности в браузере
-	music_player = get_node_or_null("MusicPlayer") as AudioStreamPlayer
+	# Получаем AudioStreamPlayer из сцены (он уже настроен в сцене)
+	music_player = get_node("MusicPlayer") as AudioStreamPlayer
 	
-	# Проверяем наличие AudioStreamPlayer из сцены
+	# Проверяем наличие AudioStreamPlayer
 	if not music_player:
 		push_error("AudioStreamPlayer не найден в сцене MusicManager")
 		return
 	
-	# Подключаем сигнал окончания трека для перезапуска
-	if not music_player.finished.is_connected(_on_music_finished):
-		music_player.finished.connect(_on_music_finished)
-	
-	# Проверяем наличие stream
+	# Проверяем наличие stream (он уже настроен в сцене)
 	if music_player.stream:
 		# Настраиваем зацикливание (должно быть уже настроено в сцене, но на всякий случай)
 		if music_player.stream is AudioStreamOggVorbis:
 			var ogg_stream: AudioStreamOggVorbis = music_player.stream as AudioStreamOggVorbis
 			ogg_stream.loop = true
+		
+		# Подключаем сигнал окончания трека для перезапуска
+		if not music_player.finished.is_connected(_on_music_finished):
+			music_player.finished.connect(_on_music_finished)
 		
 		# НЕ запускаем музыку автоматически - браузеры блокируют автозапуск
 		# Музыка будет запущена после первого пользовательского взаимодействия
@@ -50,4 +50,3 @@ func _exit_tree() -> void:
 	# Останавливаем музыку при выходе
 	if music_player and music_player.playing:
 		music_player.stop()
-
